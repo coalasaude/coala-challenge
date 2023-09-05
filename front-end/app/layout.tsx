0ed29534be6e609e0f5b271d575ab9bf5ff13190
André Glatz <andreglatz@outlook.com>
2023-09-05 15:06:28 -0300

    initial commit

---
+++ front-end/app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Wormhole',
+  description: 'Wormhole é o seu site de troca de livros',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
